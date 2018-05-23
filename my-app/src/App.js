05a264453482f440e3e1dd13c63341b7a78749ff
NhanPhan <nhan.phan@3forcom.com>
2018-05-23 16:10:50 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,27 @@ import SizeSetting from "./components/SizeSetting"
 import Reset from "./components/Reset"
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            color: 'red',
+            fontSize: 15
+        };
+        this.onSetColor = this.onSetColor.bind(this);
+    }
+
+    onSetColor = (params) => {
+        this.setState({
+            color: params
+        });
+    }
+
   render() {
     return (
         <div className="container">
             <div className="row">
-                <ColorPicker />
+                <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor} />
                 <div className="col-lg-6">
                     <SizeSetting />
                     <Reset />
--- my-app/src/components/ColorPicker.js
+++ my-app/src/components/ColorPicker.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 
 class ColorPicker extends Component {
-  render() {
-    return (
-            <div className="col-lg-6">
-                <div className="card bg-dark">
-                    <div className="card-header text-white">
-                        Color Picker
-                    </div>
-                    <div className="card-body">
-                        <span></span>
+
+    constructor(props){
+        super(props);
+        this.state = {
+            colors: ['red', 'green', 'cornflowerblue', 'gray']
+        }
+    }
+
+    showColor(color){
+        return{
+            backgroundColor: color
+        }
+    }
+
+    setActiveColor(color){
+        this.props.onReceiveColor(color);
+    }
+
+    render() {
+
+        var elmColor = this.state.colors.map((color, index) =>{
+            return <span 
+                        key={index} 
+                        style={ this.showColor(color) }
+                        className={this.props.color === color ? 'active' : ''}
+                        onClick = { () => this.setActiveColor(color)}
+                    >
+                    </span>
+        });
+
+        return (
+                <div className="col-lg-6">
+                    <div className="card bg-dark">
+                        <div className="card-header text-white">
+                            Color Picker
+                        </div>
+                        <div className="card-body">
+                            { elmColor }
+                        </div>
                     </div>
                 </div>
-            </div>
-    );
-  }
+            );
+    }
 }
 
 export default ColorPicker;
